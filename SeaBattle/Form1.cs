0ac12160e63ba5689e43442c8f257bd36bf765aa
Alexander <it.fun@mail.ru>
2025-04-23 19:49:26 +0500

    1. Сделать окрашивание клеток в зависимости от корабля. Если 4 палубы, то 4 клетки окрашиваются и т.д. 2. Сделать расстановку кораблей по вертиркали!

--- SeaBattle/Form1.cs
+++ SeaBattle/Form1.cs
@@ -12,11 +12,12 @@ namespace SeaBattle
 {
     public partial class Form1 : Form
     {
-
         int[] ships = new int[10] { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
         int count = 0;
         int[,] player_field = new int[10, 10];
         PictureBox[,] field_player_pictures = new PictureBox[10, 10];
+        PictureBox[,] field_computer_pictures = new PictureBox[10, 10]; 
+        bool dir = false; // false - гор, true - верт
         public Form1()
         {
             InitializeComponent();
@@ -28,75 +29,127 @@ namespace SeaBattle
             CreateGameField(playerField: false);
         }
 
- 
-
         private void CreateGameField(bool playerField)
         {
-            // Создание игрового поля из PictureBox
             this.Text = playerField ? "Поле игрока" : "Поле компьютера";
-            
-            int[,] field = new int[10, 10];
-            int loc_x = playerField ? 120 : 700; // Поле игрока слева, поле компьютера справа
+
+            int loc_x = playerField ? 120 : 700;
             int loc_y = 50;
 
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    field_player_pictures[i, j] = new PictureBox();
-                    field_player_pictures[i, j].Left = loc_x;
-                    field_player_pictures[i, j].Top = loc_y;
-                    field_player_pictures[i, j].Width = 50;
-                    field_player_pictures[i, j].Height = 50;
-                    field_player_pictures[i, j].Image = Properties.Resources.template;
-                    field_player_pictures[i, j].Name = "PictureBox" + i.ToString() + j.ToString();
-                    field_player_pictures[i, j].Tag = "empty";
-                    if (playerField) 
+                    PictureBox pic = new PictureBox();
+                    pic.Left = loc_x;
+                    pic.Top = loc_y;
+                    pic.Width = 50;
+                    pic.Height = 50;
+                    pic.Image = Properties.Resources.template;
+                    pic.Name = "PictureBox" + i.ToString() + j.ToString();
+                    pic.Tag = "empty";
+                    pic.SizeMode = PictureBoxSizeMode.Zoom;
+
+                    if (playerField)
                     {
-                        field_player_pictures[i, j].Click += SetShips;
+                        pic.Click += SetShips;
+                        pic.MouseHover += Picturebox_MouseHover;
+                        pic.MouseLeave += Picturebox_MouseEnd;
+                        field_player_pictures[i, j] = pic;
                     }
-                    else {
-                        field_player_pictures[i, j].Click += ShootShips;
+                    else
+                    {
+                        pic.Click += ShootShips;
+                        field_computer_pictures[i, j] = pic;
                     }
-                    field_player_pictures[i, j].SizeMode = PictureBoxSizeMode.Zoom;
-                    field_player_pictures[i, j] = field_player_pictures[i, j];
-                    this.Controls.Add(field_player_pictures[i, j]);
+                       
+
+                   
+                    this.Controls.Add(pic);
 
                     loc_x += 55;
                 }
                 loc_y += 55;
                 loc_x = playerField ? 120 : 700;
             }
-
-  
         }
 
         private void SetShips(object sender, EventArgs e)
         {
-            
+            if (count >= ships.Length)
+            {
+                MessageBox.Show("Все корабли уже расставлены.");
+                return;
+            }
+
             PictureBox picture = sender as PictureBox;
             int row = Convert.ToInt32(picture.Name[10].ToString());
             int col = Convert.ToInt32(picture.Name[11].ToString());
-            MessageBox.Show("Set ships: " + row + " " + col);
+            int shipLength = ships[count];
 
-            for (int i = 0; i < ships[count]; i++) {
-                player_field[row, col] = 1;
-                col++;
+            if (col + shipLength > 10)
+            {
+                MessageBox.Show("Корабль выходит за границы поля.");
+                return;
             }
-            
+
+            // Проверка на пересечения
+            for (int i = 0; i < shipLength; i++)
+            {
+                if (player_field[row, col + i] == 1)
+                {
+                    MessageBox.Show("На этом месте уже есть корабль.");
+                    return;
+                }
+            }
+
+            // Установка корабля
+            for (int i = 0; i < shipLength; i++)
+            {
+                player_field[row, col + i] = 1;
+                field_player_pictures[row, col + i].Image = Properties.Resources.shipset; 
+                field_player_pictures[row, col + i].Tag = "ship";
+            }
+
+            count++;
         }
 
         private void ShootShips(object sender, EventArgs e)
         {
-            MessageBox.Show("Shoot Ship");
+            MessageBox.Show("Выстрел произведён!");
         }
 
+        private void Picturebox_MouseHover(object sender, EventArgs e)
+        {
+            PictureBox picture = sender as PictureBox;
+            int row = Convert.ToInt32(picture.Name[10].ToString());
+            int col = Convert.ToInt32(picture.Name[11].ToString());
 
+            if (field_player_pictures[row, col].Tag.ToString() != "ship")
+            {
+                field_player_pictures[row, col].Image = Properties.Resources.posship;
+            }  
+        }
+        private void Picturebox_MouseEnd(object sender, EventArgs e)
+        {
 
+            PictureBox picture = sender as PictureBox;
+            int row = Convert.ToInt32(picture.Name[10].ToString());
+            int col = Convert.ToInt32(picture.Name[11].ToString());
 
-        private PictureBox[,] playerPictures;
-        private PictureBox[,] computerPictures;
-        private int[,] playerFieldState;
-        private int[,] computerFieldState;
+            if (field_player_pictures[row, col].Tag.ToString() != "ship")
+            {
+                field_player_pictures[row, col].Image = Properties.Resources.template;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.E) 
+            {
+                dir = !dir;
+                MessageBox.Show(dir.ToString());
+            }
+        }
     }
 }
